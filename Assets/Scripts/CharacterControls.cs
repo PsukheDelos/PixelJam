bf52e4b6b7117593d0040b8bb1e613651ce8fa74
Somfin <micr0softener@gmail.com>
2015-08-28 21:29:29 +1200

    Basic movement system and testing scene added.

---
+++ Assets/Scripts/CharacterControls.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControls : MonoBehaviour {
+    public float speed;
+    private float moveVert;
+    private float moveHoriz;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVert = Input.GetAxisRaw("Vertical");
+        moveHoriz = Input.GetAxisRaw("Horizontal");
+	}
+
+    void FixedUpdate()
+    {
+        transform.Translate(speed * Vector3.Normalize(new Vector3(moveHoriz, 0, moveVert)), Space.World);
+    }
+}
