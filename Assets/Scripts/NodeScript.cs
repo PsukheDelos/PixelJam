f99d154a3fccee893c6a94c7ee993663caf82feb
Glen Peek <glen.peek@gmail.com>
2015-08-29 17:50:19 +1200

    creating NodeScript and RoomScript

---
+++ Assets/Scripts/NodeScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeScript : MonoBehaviour {
+
+	public float duration;
+	public Transform[] nodes = {};
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public Transform getNextNode(){
+		return nodes[Random.Range(0, nodes.Length)];
+	}
+
+	public float getDuration(){
+		return duration;
+	}
+
+}
---
+++ Assets/Scripts/RoomScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomScript : MonoBehaviour {
+
+	public Transform[] nodes = {};
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public Transform getRandomNode(){
+		return nodes[Random.Range(0, nodes.Length)];
+	}
+
+	public Transform getNearestNode (Transform pos) {
+		var closestDistance = (nodes[0].position - pos.position).sqrMagnitude;
+		var nearestNodeIndex = 0;
+		for (var i = 1; i < nodes.Length; i++) {
+			var thisDistance = (nodes[i].position - pos.position).sqrMagnitude;
+			if (thisDistance < closestDistance) {
+				closestDistance = thisDistance;
+				nearestNodeIndex = i;
+			}
+		}
+		return nodes[nearestNodeIndex];
+	}
+}
