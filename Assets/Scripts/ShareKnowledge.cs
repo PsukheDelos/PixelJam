e80e0db41e57dadb94c5195e3c560d993638873c
Somfin <micr0softener@gmail.com>
2015-08-29 19:39:05 +1200

    Shake 'em out of their stupor!

--- Assets/Scripts/ShareKnowledge.cs
+++ Assets/Scripts/ShareKnowledge.cs
@@ -7,11 +7,14 @@ public class ShareKnowledge : MonoBehaviour {
     {
         if (c.gameObject.GetComponent<GuestKnowledge>() != null)
         {
-            Debug.DrawRay(transform.position + transform.forward, Vector3.up * 10, Color.blue);
             if (c.gameObject.GetComponent<GuestKnowledge>().canAddKnowledge(GetComponent<GuestKnowledge>().getKnowledge()))
             {
                 c.gameObject.GetComponent<GuestKnowledge>().addKnowledge(GetComponent<GuestKnowledge>().getKnowledge());
                 GetComponent<GuestState>().setState(GuestState.State.GOSSIP);
+            }
+            if (c.gameObject.GetComponent<GuestState>().getState() == GuestState.State.GOSSIP && GetComponent<GuestState>().getState() == GuestState.State.PANIC)
+            {
+                c.gameObject.GetComponent<GuestState>().setState(GuestState.State.PANIC);
             }
         }
     }
