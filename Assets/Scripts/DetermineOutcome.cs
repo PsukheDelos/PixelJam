c7e6b54a3cde148f4f49273522323c9aea56ca56
Somfin <micr0softener@gmail.com>
2015-08-30 14:34:01 +1200

    Determine outcome is a bit nicer.

--- Assets/Scripts/DetermineOutcome.cs
+++ Assets/Scripts/DetermineOutcome.cs
@@ -11,6 +11,7 @@ public class DetermineOutcome : MonoBehaviour {
 
 	public GameObject TimeUI;
 	public GameObject GameOverUI;
+    public GameObject TextOut;
 	public GameObject Player;
 	public GameObject Reticle;
 
@@ -51,7 +52,59 @@ public class DetermineOutcome : MonoBehaviour {
 		Time.timeScale = 0;
 		TimeUI.SetActive(false);
 		GameOverUI.SetActive(true);
+        TextOut.GetComponent<Text>().text = "Witness accuracy: " + (int)(score * 100) + " %\nTotal kill count: " + (totalGuests - currentGuests) + "\nGrade: " + grade();
 		Player.GetComponent<LookAtMouse>().enabled = false;
 		Reticle.GetComponent<StickToMouse>().enabled = false;
 	}
+
+    public string grade()
+    {
+        int myScore = (int)(score * 100);
+        int myKills = (totalGuests - currentGuests);
+        if (myScore < 20)
+        {
+            if (myKills < 5)
+            {
+                return "Ghost\nClean and clear, Johnny.\nJust the way we like it.";
+            }
+            else if (myKills < 15)
+            {
+                return "Assassin\nWell, you got out clean, Johnny.\nBit messy, but not bad.";
+            }
+            else
+            {
+                return "Monster\nDid you have to kill so many people, Johnny?\nJesus.";
+            }
+        }
+        else if (myScore < 80)
+        {
+            if (myKills < 5)
+            {
+                return "Knife\nHearsay and rumour.\nSafe, as far as we're concerned.";
+            }
+            else if (myKills < 15)
+            {
+                return "Murderer\nChrist, what a mess.\nAt least no-one saw much.";
+            }
+            else
+            {
+                return "Thug\nAll that blood, Johnny.\nYou are one hell of a killer.";
+            }
+        }
+        else
+        {
+            if (myKills < 5)
+            {
+                return "Pacifist\nSome day, Johnny, you're gonna have to get your hands dirty.\nSee you in twenty to life.";
+            }
+            else if (myKills < 15)
+            {
+                return "Spree Killer\nEveryone saw you, Johnny.\nDo you understand how bad that looks for us?";
+            }
+            else
+            {
+                return "Message Sender\nNo-one's gonna touch you, Johnny.\nAnd I ain't gonna defend you.";
+            }
+        }
+    }
 }
