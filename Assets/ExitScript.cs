eb21828d32910f8f20942859c46153852c74707f
Glen Peek <glen.peek@gmail.com>
2015-08-30 14:57:12 +1200

    implemented exit game over

---
+++ Assets/ExitScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitScript : MonoBehaviour {
+
+	public GameObject theJudge;
+
+	void OnTriggerEnter(Collider other) {
+		theJudge.GetComponent<DetermineOutcome> ().GameOver ();
+	}
+}
