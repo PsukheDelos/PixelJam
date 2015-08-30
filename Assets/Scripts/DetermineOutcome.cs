b517ff755913cccee02a05858b44334971d13fc8
Glen Peek <glen.peek@gmail.com>
2015-08-30 13:12:29 +1200

    implemented game timer

--- Assets/Scripts/DetermineOutcome.cs
+++ Assets/Scripts/DetermineOutcome.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DetermineOutcome : MonoBehaviour {
@@ -6,19 +7,27 @@ public class DetermineOutcome : MonoBehaviour {
     private float score;
     private int totalGuests;
     private int currentGuests;
+	private float time;
+	public GameObject TimeUI;
+
 
 	// Use this for initialization
 	void Start () {
         totalGuests = GameObject.FindGameObjectsWithTag("Guest").Length;
+		time = 300f; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		time -= Time.deltaTime;
         score = 0;
         currentGuests = GameObject.FindGameObjectsWithTag("Guest").Length;
 	    foreach (GameObject o in GameObject.FindGameObjectsWithTag("Guest")){
             score = Mathf.Max(score, o.GetComponent<GuestKnowledge>().getKnowledge());
         }
+		int minutes = Mathf.FloorToInt(time / 60F);
+		int seconds = Mathf.FloorToInt(time - minutes * 60);
+		TimeUI.GetComponent<Text> ().text = string.Format("{0:0}:{1:00}", minutes, seconds);
 	}
 
     void OnGUI () {
